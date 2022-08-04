a8cd12de041a847061b8e9a82799739510fb731b
C M Hai <cmh.fed@gmail.com>
2022-08-04 16:40:12 +0700

    fix css

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="">
+    <div className="w-100">
       <div className="about1 p-5">
         <h3 className="text-center mb-3">Fur-shop ...</h3>
         <div>
@@ -82,7 +82,7 @@ const About = () => {
         </div>
       </div>
 
-      <div >
+      <div className="map">
         <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59587.94583109359!2d105.80194392473273!3d21.022816135798717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab9bd9861ca1%3A0xe7887f7b72ca17a9!2zSMOgIE7hu5lpLCBIb8OgbiBLaeG6v20sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1659604888924!5m2!1svi!2s" width="600" height="450" style={{ border: '0' }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
